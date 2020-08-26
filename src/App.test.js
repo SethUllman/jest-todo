d7d2feeedb0b12eea2b189bea6d8ba99ec8725b6
Seth Ullman <seth.ullman98@gmail.com>
2020-08-26 17:54:50 -0600

    add first tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Account from "./Account";
+import App from "./App";
+import toJson from "enzyme-to-json";
+import { isMainThread } from "worker_threads";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  shallow(<App />);
 });
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Display Active Users Account Details</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+})
