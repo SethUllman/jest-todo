ad3e18d6c44d93edd78fac124f7d2f0ba153aac7
Seth Ullman <seth.ullman98@gmail.com>
2020-08-30 17:42:43 -0600

    add <Account /> testing

--- src/App.test.js
+++ src/App.test.js
@@ -9,8 +9,33 @@ it("renders without crashing", () => {
   shallow(<App />);
 });
 
+it("renders correctly with no error message", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.state("error")).toEqual(null);
+});
+
 it("renders Account header", () => {
   const wrapper = shallow(<App />);
   const welcome = <h1>Display Active Users Account Details</h1>;
   expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+
+const user = {
+  name: 'Emerald Hart',
+  email: 'emeraldhart@outlook.com',
+  username: 'Maverick2018',
+}
+
+describe("<Account />", () => {
+  it("accepts user account props", () => {
+    const wrapper = mount(<Account user={user} />);
+    expect(wrapper.props().user).toEqual(user);
+  });
+  it("contains users account email", () => {
+    const wrapper = mount(<Account user={user} />);
+    const value = wrapper.find("p").text();
+    expect(value).toEqual("emeraldhart@outlook.com");
+  });
+  
 })
